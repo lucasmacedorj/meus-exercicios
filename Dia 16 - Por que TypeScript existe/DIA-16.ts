6b50fd48069e32b965d6c3a508fad7143e099066
Leandro Lucas Macedo Dos Santos <leandrolucas60@gmail.com>
2026-08-11 17:47:28 -0300

    Dia 16 - Por que TypeScript existe

---
+++ Dia 16 - Por que TypeScript existe/DIA-16.ts
@@ -0,0 +1,10 @@
+let idadeMinima: number = 18
+let idadeUsuario: number = 18
+
+
+if (idadeMinima === idadeUsuario) {
+    console.log("Idade permitida")
+} else {
+    console.log("Idade proibida")
+}
+
